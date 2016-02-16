858481f724d224a6c65bded4396a7753fdb4a2f4
Sean Lobo <seanlobo2010@gmail.com>
2016-02-16 13:41:47 -0800

    Add test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+# Place to run all commands
+
+from functions.messagereader import MessageReader 
+from functions.convoreader import ConvoReader
+from functions.customdate import CustomDate
+
+
+
+m = MessageReader()
+
+
