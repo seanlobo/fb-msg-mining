fd1c3d6014df9a641bfc44ad4efc8ba0da3a2459
Sean Lobo <seanlobo2010@gmail.com>
2016-06-04 19:11:19 -0700

    Add initial help to playground

--- playground.py
+++ playground.py
@@ -7,3 +7,10 @@ from functions import emojis
 
 m = MessageReader()
 
+print("\nWelcome to the python3 terminal version of fb-msg-mining!\n\n")
+print("If you are unfamiliar with python syntax, consider getting up to speed by executing the command \"help()\"\n")
+
+print('Once you feel comfortable with basic python syntax, get ready to analyze your conversations with the '
+      'command \"m.help()\"\n\n')
+
+
