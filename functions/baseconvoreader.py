61eeb3e7065c21c1b6129348ef5990ae8494f2e7
Sean Lobo <seanlobo2010@gmail.com>
2016-06-10 14:09:18 -0700

    Add initial wordCloud support to BaseConvoReader
    Add infrastructure for creating files, checking input before creation,
    and verifying files are created (for use before calling word cloud
    creation). So far only uses parameters for a "circular" wordcloud

--- functions/baseconvoreader.py
+++ functions/baseconvoreader.py
@@ -72,10 +72,10 @@ class BaseConvoReader:
                 people.append(person.lower())
         return sorted(people)
 
-    def save_word_freq(self):
+    def save_word_freq(self, path=None):
         """Saves to a file the ordered rankings of word frequencies by person in the chat"""
-
-        os.makedirs(self._path[0:-1], exist_ok=True)
+        path = self._path if path is None else path
+        os.makedirs(path[0:-1], exist_ok=True)
         for person, counter in self._individual_words.items():
             split = person.split()
             pers = ''
@@ -83,7 +83,7 @@ class BaseConvoReader:
                 pers += split[i]
                 pers += '-'
             pers += split[-1]
-            with open(self._path + pers + '_word_freq.txt', mode='w', encoding='utf-8') as f:
+            with open(path + pers + '_word_freq.txt', mode='w', encoding='utf-8') as f:
                 lst = []
                 for key, val in counter.items():
                     lst.append((key, val))
@@ -92,7 +92,7 @@ class BaseConvoReader:
         count = Counter()
         for key, val in self._individual_words.items():
             count += val
-        with open(self._path + 'total.txt', mode='w', encoding='utf-8') as f:
+        with open(path + 'total.txt', mode='w', encoding='utf-8') as f:
             for key, val in count.most_common():
                 f.write("{0}: {1}".format(key, val) + "\n")
 
@@ -129,6 +129,100 @@ class BaseConvoReader:
         with open(output, mode='x') as f:
             f.write(string)
         print('done\a')
+
+    def verify_word_cloud_setup(self, type="circular"):
+        """Verifies that all the settings for a word cloud are met"""
+        verification = dict()
+        if type == "circular":
+            assertions = [self._assert_dimensions_for_wc, self._assert_num_text_set_for_wc,
+                          self._assert_color_for_wc, self._assert_output_name_for_wc]
+            file_names = ['dimensions.txt', 'num_text_sets.txt', 'colors.txt', 'output_name.txt']
+            fn = lambda num: assertions[num]
+            for i in range(4):
+                try:
+                    assert os.path.isfile(BaseConvoReader.word_cloud_path + file_names[i]), \
+                        "{0} is missing".format(file_names[i])
+                    with open(BaseConvoReader.word_cloud_path + file_names[i], mode='r') as f:
+                        if i == 0:      # dimension
+                            val = [int(ele) for ele in f.read().split('\n')]
+                        elif i == 1:    # number of text sets
+                            val = int(f.read())
+                        elif i == 2:    # colors
+                            val = [[int(col) for col in line.split(', ')] for line in f]
+                        else:           # output name
+                            val = f.read()
+                    if i != 2:
+                        fn(i)(val)
+                    else:
+                        [fn(2)(data) for data in val]
+                except (AssertionError, ValueError) as e:
+                    verification[file_names[i]] = e
+        print(verification)
+
+    @staticmethod
+    def set_dimentions(x, y):
+        BaseConvoReader._assert_dimensions_for_wc([x, y])
+
+        with open(BaseConvoReader.word_cloud_path + 'dimensions.txt', mode='w', encoding='utf-8') as f:
+            f.write(str(x) + '\n' + str(y))
+
+    @staticmethod
+    def set_num_word_sets(num):
+        BaseConvoReader._assert_num_text_set_for_wc(num)
+
+        with open(BaseConvoReader.word_cloud_path + 'num_text_sets.txt', mode='w', encoding='utf-8') as f:
+            f.write(str(num))
+
+    @staticmethod
+    def set_colors(color):
+        """Writes the passed colors to the current colors.txt file"""
+        BaseConvoReader._assert_color_for_wc(color)
+
+        with open(BaseConvoReader.word_cloud_path + 'colors.txt', mode='w', encoding='utf-8') as f:
+            f.write('{0}, {1}, {2}\n'.format(*color))
+
+    @staticmethod
+    def append_color(color):
+        """Appends the set colors to colors.txt if colors exists otherwise calls set_colors"""
+        BaseConvoReader._assert_color_for_wc(color)
+        if os.path.isfile(BaseConvoReader.word_cloud_path + 'colors.txt'):
+            with open(BaseConvoReader.word_cloud_path + 'colors.txt', mode='a', encoding='utf-8') as f:
+                f.write('{0}, {1}, {2}\n'.format(*color))
+        else:
+            BaseConvoReader.set_colors(color)
+
+    @staticmethod
+    def set_output_name(name):
+        """Writes the passed name to output_name.txt"""
+        BaseConvoReader._assert_output_name_for_wc(name)
+
+        with open(BaseConvoReader.word_cloud_path + 'output_name.txt', mode='w', encoding='utf-8') as f:
+            f.write(name)
+
+    @staticmethod
+    def _assert_dimensions_for_wc(dimensions):
+        assert isinstance(dimensions, list) or isinstance(dimensions, tuple), "Dimensions is an invalid type"
+        x, y = dimensions
+        assert isinstance(x, int), "X needs to be an integer"
+        assert isinstance(y, int), "Y needs to be an integer"
+        assert 0 < x, "X must be greater than 0"
+        assert 0 < y, "Y must be greater than 0"
+
+    @staticmethod
+    def _assert_color_for_wc(color):
+        assert (isinstance(color, tuple) or isinstance(color, list)) and all(isinstance(ele, int) for ele in color), \
+            "Color must be a tuple (or list) with 3 ints, e.g. (100, 100, 100)"
+        assert all(0 <= val <= 255 for val in color), "All rgb values must be between 0 and 255"
+
+    @staticmethod
+    def _assert_output_name_for_wc(name):
+        assert isinstance(name, str), "Name should be a string"
+        assert '.png' in name, "Name should be a .png file"
+
+    @staticmethod
+    def _assert_num_text_set_for_wc(num):
+        assert isinstance(num, int), "Num must be an integer"
+        assert num >= 1, "Num should be greater than 1"
 
     def _raw_messages(self, name=None):
         """Number of messages for people in the chat
@@ -357,28 +451,10 @@ class BaseConvoReader:
 
         shutil.rmtree(BaseConvoReader.word_cloud_path) if os.path.exists(BaseConvoReader.word_cloud_path) else None
         os.makedirs(BaseConvoReader.word_cloud_path, exist_ok=True)
-        with open(BaseConvoReader.word_cloud_path + 'convo_name.txt', mode='w', encoding='utf-8') as f:
-            f.write(self._path)
         if os.path.isfile(self._path + 'excludedWords.txt'):
             shutil.copyfile(self._path + 'excludedWords.txt', BaseConvoReader.word_cloud_path + 'excludedWords.txt')
-        if not os.path.isfile(self._path + 'total.txt'):
-            self.save_word_freq()
-        shutil.copyfile(self._path + 'total.txt', BaseConvoReader.word_cloud_path + 'total.txt')
-
-    @staticmethod
-    def _is_hex_color_code(code):
-        r = re.compile('^#(?:[0-9a-fA-F]{3}){1,2}$')
-        return r.match(code) is not None
-
-    def word_clouds(self):
-        self._setup_word_cloud()
-
-    def set_colors(self, *colors):
-        for color in colors:
-            assert BaseConvoReader._is_hex_color_code(color), "{0} is not a valid Hexadecimal color code".format(color)
-        with open(BaseConvoReader.word_cloud_path + 'colors.txt', mode='w', encoding='utf-8') as f:
-            for color in colors:
-                f.write(color + '\n')
+        if not os.path.isfile(BaseConvoReader.word_cloud_path + 'total.txt'):
+            self.save_word_freq(path=BaseConvoReader.word_cloud_path)
 
     def _find_indexes(self, query, ignore_case=False):
         """Returns a list with the indexes of each message that contain the passed message
